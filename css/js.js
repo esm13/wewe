a4f287eae60ceb28120e59e7a81bc2592dc00b82
esm13 <esmira.agayeva.13@mail.ru>
2022-10-25 20:05:58 +0400

    sdsdsd

---
+++ css/js.js
@@ -0,0 +1,14 @@
+
+const elem=document.querySelector(".box .pic")
+function col(){
+    document.getElementById("box").style.backgroundImage = "pic"
+    
+}
+
+function olcu(){
+    document.getElementById("box").style.width="400px"
+}
+
+function anim(){
+    document.getElementById("box").style.animationName="mymove"
+}
